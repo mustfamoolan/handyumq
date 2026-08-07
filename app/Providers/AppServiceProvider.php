4c10de5c819a8b0a903116e2f1aa4f83b14c3b4b
mustfamoolan <samiraalyaseen20@gmail.com>
2026-08-07 17:51:48 +0300

    Fix duplicate migrations causing server crash

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (request()->server('HTTP_X_FORWARDED_PROTO') === 'https' || request()->isSecure() || config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
